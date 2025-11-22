d7124d797e71d0d019250e5ba404473479273906
Mathis Nedeau <matnedeau@gmail.com>
2025-11-22 19:59:55 +0100

    Title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Español Sprint - B2 en 90 Jours",
+  title: "EspaSprint",
   description: "Apprends l'espagnol rapidement avec la méthode Pareto et la répétition espacée.",
 };
 
