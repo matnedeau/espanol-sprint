f5f6959afd009e9dd5996729873c02233093e09d
Mathis Nedeau <matnedeau@gmail.com>
2025-11-23 21:17:40 +0100

    VF

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Español Sprint - B2 en 90 Jours",
+  title: "EspañolSprint",
   description: "L'application d'apprentissage de l'espagnol la plus rapide.",
   // Note : Next.js détecte automatiquement le fichier icon.png dans le dossier app/
   // On n'a rien à ajouter ici !
